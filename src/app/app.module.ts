8844b1988f9a03deedbaae735cef0a9e6376541c
RaphaelCpp <cappiello.raphael@gmail.com>
2022-03-21 19:58:10 +0100

    last on touche plus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MailComponent } from './component/mail/mail.component';
-import { SliderComponent } from './component/slider/slider.component';
 import { HeaderComponent } from './component/static/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, MailComponent, SliderComponent, HeaderComponent],
+  declarations: [AppComponent, MailComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
